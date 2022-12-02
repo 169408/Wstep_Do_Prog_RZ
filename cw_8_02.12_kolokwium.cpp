72be3497bf398e504332ace47b09832c496fceba
Ruslan <115550042+169408@users.noreply.github.com>
2022-12-02 15:58:43 +0100

    Add files via upload

---
+++ cw_8_02.12_kolokwium.cpp
@@ -0,0 +1,148 @@
+﻿// cw_8_02.12_kolokwium.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Zadanie 1
+int secondToMax(int arg[], int length) {
+    for (int j = 0; j < length; j++) {
+        for (int k = 0; k < length - 1 - j; k++) {
+            if (arg[k] > arg[k + 1]) {
+                int temp = arg[k];
+                arg[k] = arg[k + 1];
+                arg[k + 1] = temp;
+            }
+        }
+    }
+
+    return arg[length - 2];
+}
+
+// Zadanie 2
+string reversedText(string arg) {
+    string reversed = arg;
+    int sLength = arg.length();
+    for (int i = 0; i < sLength; i++) {
+        reversed[i] = arg[sLength - 1 - i];
+    }
+
+    return reversed;
+}
+
+//Zadanie 3
+int gcd(int arg1, int arg2) {
+    int lengthNum;
+    int dzielnik = 1;
+    if (arg1 >= arg2) {
+        lengthNum = arg1;
+    } else {
+        lengthNum = arg2;
+    }
+
+    for (int s = 2; s < lengthNum; s++) {
+        if (arg1 % s == 0 && arg2 % s == 0) {
+            dzielnik = s;
+        }
+    }
+
+    return dzielnik;
+
+}
+
+// Zadanie 4
+
+void randomNumbers() {
+    int result = 0;
+    int count5 = 0, count1 = 0;
+    srand(time(NULL));
+    while (count5 < 3 || count1 < 2) {
+        result = rand() % 11 - 5;
+        if (result == 5) {
+            count5++;
+            cout << result << " ";
+        }
+        else if (result == 1) {
+            count1++;
+            cout << result << " ";
+        }
+    }
+    cout << endl;
+}
+
+void dokladnie() {
+    int res = 0;
+    int num5 = 0, num1 = 0;
+    srand(time(NULL));
+    while (num5 < 3 || num1 < 2) {
+        res = rand() % 11 - 5;
+        if (res == 5) {
+            if (num5 < 3) {
+                num5++;
+                cout << res << " ";
+            }
+        }
+        else if (res == 1) {
+            if (num1 < 2) {
+                num1++;
+                cout << res << " ";
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // Zadanie 1
+
+    int arr[20];
+    srand(time(NULL));
+    int length = sizeof(arr) / sizeof(int);
+    for (int i = 0; i < size(arr); i++) {
+        arr[i] = rand() % 101;
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << secondToMax(arr, length) << endl;
+
+    // Zadanie 2
+
+    string slovo;
+    cout << "wpish slovo : ";
+    cin >> slovo;
+
+    cout << reversedText(slovo) << endl;
+
+    // Zadanie 3
+
+    int num1, num2;
+    cout << "wpisz dwie calkowity liczby: ";
+    cin >> num1;
+    cin >> num2;
+
+    cout << "The greatest common divisor of " << num1 << " and " << num2 << " is " << gcd(num1, num2) << endl;
+
+    // Zadanie 4
+
+    randomNumbers();
+    dokladnie();
+
+
+
+    return 0;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
